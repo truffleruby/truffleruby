04e97dd42ece5ca89293ba7301281c5ac26e09ef
Benoit Daloze <eregontp@gmail.com>
2025-10-03 11:11:23 +0200

    Run `jt test ecosystem discourse` without the gem_test_pack
    * Update discourse gems as necessary.

--- test/truffle/ecosystem/discourse.sh
+++ test/truffle/ecosystem/discourse.sh
@@ -1,25 +1,13 @@
 #!/usr/bin/env bash
 
-SELF_PATH=$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0")
-while [ -h "$SELF_PATH" ]; do
-  DIR=$(dirname "$SELF_PATH")
-  SYM=$(readlink "$SELF_PATH")
-  SELF_PATH=$(cd "$DIR" && cd "$(dirname "$SYM")" && pwd)/$(basename "$SYM")
-done
+source test/truffle/common.sh.inc
 
-ecosystem=$(dirname "$SELF_PATH")
-truffle=$(dirname "$ecosystem")
-# shellcheck source=test/truffle/common.sh.inc
-source "$truffle/common.sh.inc"
+ruby_home="$(jt ruby-home)"
+export PATH="$ruby_home/bin:$PATH"
 
-alias truffleruby="jt ruby -S"
+cd test/truffle/ecosystem/discourse || exit 2
 
-gem_test_pack_path="$(jt gem-test-pack)"
-
-cd "$gem_test_pack_path/gem-testing/discourse-2.3.4" || exit 2
-
-# Only works on x86_64-linux, as it needs a precompiled gem, gem-cache/libv8-*-x86_64-linux.gem
-if jt ruby -e 'abort unless RUBY_PLATFORM.include?("x86_64-linux")'; then
-  echo 'Disabled due to https://github.com/rubygems/rubygems/issues/6165'
-  # truffleruby bundle install
-fi
+echo 'Disabled due to https://github.com/rubygems/rubygems/issues/6165'
+export BUNDLE_PATH=vendor/bundle
+bundle lock
+bundle install
