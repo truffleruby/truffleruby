6546ea253f801071468c29d9a9a3d86c236a3e0c
Kevin Menard <kevin.menard@shopify.com>
2025-11-25 04:25:26 -0500

    Apply patch to the 'rbs' gem to work with TruffleRuby.
    The 'rbs' source layout changed somewhere between 3.4.0 and 3.8.0 so we needed to adapt the Makefile for the new source directories.

--- lib/gems/gems/rbs-3.8.0/ext/rbs_extension/extconf.rb
+++ lib/gems/gems/rbs-3.8.0/ext/rbs_extension/extconf.rb
@@ -1,14 +1,21 @@
 require 'mkmf'
 
-$INCFLAGS << " -I$(top_srcdir)" if $extmk
-$INCFLAGS << " -I$(srcdir)/../../include"
+if defined?(::TruffleRuby)
+  # On TruffleRuby, all .c files are copied to the same directory (src/main/c/rbs)
+  # and include files are copied to an include/ subdirectory
+  $INCFLAGS << " -I$(srcdir)/include"
+  $srcs = Dir.glob("#{__dir__}/*.c")
+else
+  $INCFLAGS << " -I$(top_srcdir)" if $extmk
+  $INCFLAGS << " -I$(srcdir)/../../include"
 
-$VPATH << "$(srcdir)/../../src"
-$VPATH << "$(srcdir)/ext/rbs_extension"
+  $VPATH << "$(srcdir)/../../src"
+  $VPATH << "$(srcdir)/ext/rbs_extension"
 
-root_dir = File.expand_path('../../../', __FILE__)
-$srcs = Dir.glob("#{root_dir}/src/*.c") +
-        Dir.glob("#{root_dir}/ext/rbs_extension/*.c")
+  root_dir = File.expand_path('../../../', __FILE__)
+  $srcs = Dir.glob("#{root_dir}/src/*.c") +
+          Dir.glob("#{root_dir}/ext/rbs_extension/*.c")
+end
 
 append_cflags ['-std=gnu99']
 create_makefile 'rbs_extension'
