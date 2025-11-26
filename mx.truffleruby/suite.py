31aa23ad2fb2f962098297e5ab881e0021ca8ea9
Kevin Menard <kevin.menard@shopify.com>
2025-11-26 00:47:10 -0500

    Add gem.build.complete markers for native gems promoted from default to bundled.

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -874,9 +874,12 @@ suite = {
                     "dependency:org.truffleruby.cext/src/main/c/cext-trampoline/<lib:trufflerubytrampoline>",
                 ],
                 # Create the complete files to let RubyGems know the gems are fully built and can be activated
+                "lib/gems/extensions/<cruby_arch>-<os>/<truffleruby_abi_version>/bigdecimal-3.1.8/gem.build_complete": "string:",
                 "lib/gems/extensions/<cruby_arch>-<os>/<truffleruby_abi_version>/debug-1.11.0/gem.build_complete": "string:",
+                "lib/gems/extensions/<cruby_arch>-<os>/<truffleruby_abi_version>/nkf-0.2.0/gem.build_complete": "string:",
                 "lib/gems/extensions/<cruby_arch>-<os>/<truffleruby_abi_version>/racc-1.8.1/gem.build_complete": "string:", # actually we do not build the C extension because the pure-Ruby fallback is enough
                 "lib/gems/extensions/<cruby_arch>-<os>/<truffleruby_abi_version>/rbs-3.8.0/gem.build_complete": "string:",
+                "lib/gems/extensions/<cruby_arch>-<os>/<truffleruby_abi_version>/syslog-0.2.0/gem.build_complete": "string:",
                 "lib/gems/gems/debug-1.11.0/lib/debug/": [
                     "dependency:org.truffleruby.cext/src/main/c/debug/<extsuffix:debug>",
                 ],
