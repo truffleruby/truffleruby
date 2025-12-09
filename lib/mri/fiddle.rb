ea31f56e433629b7b286b3cb1e402989405ae7f9
Benoit Daloze <eregontp@gmail.com>
2025-12-09 16:49:29 +0100

    Use the FFI-based Fiddle backend from the gem and remove our own
    * It is more compatible and better-tested.

--- lib/mri/fiddle.rb
+++ lib/mri/fiddle.rb
@@ -3,9 +3,7 @@
 if RUBY_ENGINE == 'ruby'
   require 'fiddle.so'
 else
-  # TODO (nirvdrum 2025-11-23) Can we upstream our changes? There's already support for alternative Rubies.
-  require 'truffle/fiddle_backend'
-  # require 'fiddle/ffi_backend'
+  require 'fiddle/ffi_backend'
 end
 require 'fiddle/closure'
 require 'fiddle/function'
