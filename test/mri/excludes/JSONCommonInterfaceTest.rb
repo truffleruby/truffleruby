1e2560db9db4677fdb9ba92f88e77c0ebaed2c28
Benoit Daloze <eregontp@gmail.com>
2025-12-09 14:46:59 +0100

    JSON tests are all passing now

--- test/mri/excludes/JSONCommonInterfaceTest.rb
+++
@@ -1,4 +0,0 @@
-exclude :test_JSON, "TypeError: nil is not like a string"
-exclude :test_dump, "SystemStackError: stack level too deep"
-exclude :test_index, "TypeError: nil is not like a string"
-exclude :test_load_file_with_bad_default_external_encoding, "Encoding::InvalidByteSequenceError: \"\\xE2\" on US-ASCII"
