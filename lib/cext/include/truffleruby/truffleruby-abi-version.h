b9285ba630dc77ef1bb2ff1002d83d486de69786
Herwin <herwinw@users.noreply.github.com>
2026-01-18 18:36:30 +0100

    Bump TRUFFLERUBY_ABI_VERSION

--- lib/cext/include/truffleruby/truffleruby-abi-version.h
+++ lib/cext/include/truffleruby/truffleruby-abi-version.h
@@ -25,6 +25,6 @@
 // and cannot reach the same version while having different ABI
 // ($TRUFFLERUBY_VERSION is never the same as $RUBY_VERSION).
 
-#define TRUFFLERUBY_ABI_VERSION "3.4.7.5"
+#define TRUFFLERUBY_ABI_VERSION "3.4.7.6"
 
 #endif
