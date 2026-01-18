2419b1517939ca02ecd8f7cd943cb478ae40340b
Benoit Daloze <eregontp@gmail.com>
2026-01-18 13:34:01 +0100

    Set ABI version for release branch

--- lib/cext/include/truffleruby/truffleruby-abi-version.h
+++ lib/cext/include/truffleruby/truffleruby-abi-version.h
@@ -25,6 +25,6 @@
 // and cannot reach the same version while having different ABI
 // ($TRUFFLERUBY_VERSION is never the same as $RUBY_VERSION).
 
-#define TRUFFLERUBY_ABI_VERSION "33.0.0.1"
+#define TRUFFLERUBY_ABI_VERSION "33.0.1.1"
 
 #endif
