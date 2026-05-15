d437b9ca38257dd5564917a4eaf144a572a02dfa
Benoit Daloze <eregontp@gmail.com>
2026-05-15 22:19:42 +0200

    [WIP] Implement IO::Buffer

--- src/main/java/org/truffleruby/core/CoreLibrary.java
+++ src/main/java/org/truffleruby/core/CoreLibrary.java
@@ -1102,6 +1102,7 @@ public final class CoreLibrary {
             "/core/truffle/polyglot.rb",
             "/core/truffle/polyglot_methods.rb",
             "/core/posix.rb",
+            "/core/io_buffer.rb",
             "/core/data.rb",
             "/core/truffle/queue_operations.rb",
             "/core/pathname.rb",
