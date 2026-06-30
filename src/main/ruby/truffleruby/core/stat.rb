7ad3d4f9d857e148fe1b62be9c50ab2e0c6ce969
Andrii Konchyn <andry.konchin@gmail.com>
2026-06-30 23:29:38 +0300

    Fix path encoding in File.{grpowned?,lstat},  File::Stat#{blockdev?,chardev?,directory?,executable_real?,executable?,file?,grpowned?,owned?,pipe?,readable_real?,readable?,setgid?,setuid,socket?,sticky?,symlink?,writable_real?,writable?,zero?,},  File::Stat.{size?,size,world_readable?,world_writable?,}

--- src/main/ruby/truffleruby/core/stat.rb
+++ src/main/ruby/truffleruby/core/stat.rb
@@ -94,6 +94,7 @@ class File
 
     def self.lstat?(path)
       path = Truffle::Type.coerce_to_path(path)
+      path = Truffle::Type.coerce_path_encoding(path)
       Truffle::FFI::MemoryPointer.new(:uint32, BUFFER_SIZE) do |ptr|
         if Truffle::POSIX.truffleposix_lstat(path, ptr) == 0
           Stat.new ptr
