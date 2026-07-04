781863a0f305bd03c1e7fadf6ee293b07aefefc5
Earlopain <14981592+Earlopain@users.noreply.github.com>
2026-07-04 14:36:44 +0200

    Warn for all deprecated global variable setters
    Only `$,` already did.
    
    `$/` is non-nil by default, so directly use `global_variable_set` to skip the hook.
    Same for $\ when -l is given.

--- src/main/ruby/truffleruby/core/truffle/kernel_operations.rb
+++ src/main/ruby/truffleruby/core/truffle/kernel_operations.rb
@@ -80,10 +80,11 @@ module Truffle
         if v && !Primitive.is_a?(v, String)
           raise TypeError, "value of #{name} must be String"
         end
+        warn "non-nil '#{name}' is deprecated", uplevel: 1, category: :deprecated if !Primitive.nil?(v)
         Primitive.global_variable_set :$/, v ? -v.to_s : v
       })
 
-    $/ = "\n".freeze
+    Primitive.global_variable_set :$/, "\n".freeze
 
     define_hooked_variable(
       :$\,
@@ -92,6 +93,7 @@ module Truffle
         if v && !Primitive.is_a?(v, String)
           raise TypeError, "value of #{name} must be String"
         end
+        warn "non-nil '#{name}' is deprecated", uplevel: 1, category: :deprecated if !Primitive.nil?(v)
         Primitive.global_variable_set :$\, v
       })
 
@@ -99,7 +101,7 @@ module Truffle
 
     Truffle::Boot.delay do
       if Truffle::Boot.get_option 'chomp-loop'
-        $\ = $/
+        Primitive.global_variable_set :$\, $/
       end
     end
 
@@ -112,7 +114,7 @@ module Truffle
         if v && !Primitive.is_a?(v, String)
           raise TypeError, "value of #{name} must be String"
         end
-        warn "'#{name}' is deprecated", uplevel: 1 if !Primitive.nil?(v) && Warning[:deprecated]
+        warn "non-nil '#{name}' is deprecated", uplevel: 1, category: :deprecated if !Primitive.nil?(v)
         Primitive.global_variable_set :$,, v
       })
 
