bcae0dfe61d14a9265d241c0bc0e5c2527f8b89f
Benoit Daloze <eregontp@gmail.com>
2026-07-17 23:51:14 +0200

    Update to graal with no warnings on ECJ 3.46.0

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -21,7 +21,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "d9de8206fff02d7a99bf82043b5ba8b392aa09ee",
+                "version": "1d3ce3222e3a77a2115683e5f67547d4e724a2d4",
                 "urls": [
                     {"url": "https://github.com/truffleruby/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -30,7 +30,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "d9de8206fff02d7a99bf82043b5ba8b392aa09ee",
+                "version": "1d3ce3222e3a77a2115683e5f67547d4e724a2d4",
                 "urls": [
                     {"url": "https://github.com/truffleruby/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
