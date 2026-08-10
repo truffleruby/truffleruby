130c61bb40d81fd07e01e1c1fc7bbb4b871c10be
Andrii Konchyn <andry.konchin@gmail.com>
2026-08-10 22:13:22 +0300

    Fix IO#ungetc when pushed-back string exceeds consumed bytes in buffer

--- src/main/ruby/truffleruby/core/io.rb
+++ src/main/ruby/truffleruby/core/io.rb
@@ -290,18 +290,20 @@ class IO
     # Prepends the bytes of string +str+ to the internal buffer,
     # so that future reads will return them.
     def put_back(str)
-      length = str.bytesize
-      # A simple case, which is common and can be done efficiently
-      if @start >= length
-        @start -= length
-        str.bytes.each_with_index do |byte, i|
-          @storage[@start+i] = byte
+      TruffleRuby.synchronized(self) do
+        length = str.bytesize
+        # A simple case, which is common and can be done efficiently
+        if @start >= length
+          @start -= length
+          @storage.fill(@start, str, 0, length)
+        else
+          unread = Primitive.string_from_bytearray(@storage, @start, size, Encoding::BINARY)
+          prepended_str = str.b + unread
+          @storage = Truffle::ByteArray.new(0).prepend(prepended_str)
+          @total = prepended_str.bytesize
+          @start = 0
+          @used = @total
         end
-      else
-        @storage = @storage.prepend(str)
-        @total += length
-        @start = 0
-        @used += length
       end
     end
 
