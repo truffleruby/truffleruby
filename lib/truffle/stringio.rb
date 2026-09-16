aa4bc0bcad9087f3eb98f4347e8fc52d966ba88a
Andrii Konchyn <andry.konchin@gmail.com>
2026-09-16 23:39:55 +0300

    Bump StringIO::VERSION from 3.0.1 to 3.2.0 to match default gem version for Ruby 4.0

--- lib/truffle/stringio.rb
+++ lib/truffle/stringio.rb
@@ -105,7 +105,7 @@ end
 
 class StringIO
 
-  VERSION = '3.0.1' # Same version as the default gem in CRuby 3.1.3
+  VERSION = '3.2.0' # Same version as the default gem in CRuby 4.0.2
 
   include Enumerable
   include Truffle::CExt.rb_define_module_under(IO, 'generic_readable')
